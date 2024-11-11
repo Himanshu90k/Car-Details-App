7c6d6a03aa08f57f3c29b5b72a7ed06fd2ad62ce
Himanshu Rawat <himanshurawat90k@gmail.com>
2024-11-11 10:35:42 +0530

    add dynamic html tag for car card component

--- src/components/CarCard.tsx
+++ src/components/CarCard.tsx
@@ -20,12 +20,15 @@ const CarCard: React.FC<CarCardProps> = ({car, index}) => {
     //different margin for single view car details page
     const location = useLocation()
     const marginTop = location.pathname === '/'? 'mt-4':'mt-1'
+    const dynamicHtmlTag = location.pathname !== `/car-details/${index}`? true : false
 
     return (
-        <Link 
+        dynamicHtmlTag ? 
+        (<Link 
             to={`car-details/${index}`}
             title='single view car details page'
-            className={`flex flex-col items-center w-86 h-44 rounded-4-xl border-2 border-white border-solids ${bgColor} shadow-cardShadow ${marginTop}`}>
+            className={`flex flex-col items-center w-86 h-44 rounded-4-xl border-2 border-white border-solids ${bgColor} shadow-cardShadow ${marginTop}`}
+        >
 
             <div className=" flex justify-between items-center mt-2.5 w-80 h-8 rounded-3xl px-8 bg-white">
                 {/* car name */}
@@ -68,6 +71,57 @@ const CarCard: React.FC<CarCardProps> = ({car, index}) => {
                 <p className="font-montserrat font-semibold text-white text-xxs">{car.work}</p>
             </div>
         </Link>
+        
+        ) : 
+
+        (<div
+            className={`flex flex-col items-center w-86 h-44 rounded-4-xl border-2 border-white border-solids ${bgColor} shadow-cardShadow ${marginTop}`}
+        >
+
+            <div className=" flex justify-between items-center mt-2.5 w-80 h-8 rounded-3xl px-8 bg-white">
+                {/* car name */}
+                <h2 className="text-customGrey text-xs font-extrabold font-montserrat">{car.carName}</h2>
+
+                {/* car number */}
+                <h2 className="text-customGrey text-xs font-extrabold font-montserrat">{car.carNo}</h2>
+            </div>
+
+            <div className="flex flex-col gap-2.5 w-full pl-10 mt-4">
+                {/* mechanic name */}
+                <div className="flex items-center gap-8">
+                    {/* label */}
+                    <h3 className="text-xxs font-montserrat font-bold text-white w-22">Mechanic Name</h3>
+                    <hr className="w-2 h-0.5 border-white"/>
+                    {/* value */}
+                    <p className="text-xxs font-montserrat font-bold text-white">{car.mechanicName}</p>
+                </div>
+
+                {/* service advisor */}
+                <div className="flex items-center gap-8">
+                    {/* label */}
+                    <h3 className="text-xxs font-montserrat font-bold text-white w-22">Service Advisor</h3>
+                    <hr className="w-2 h-0.5 border-white"/>
+                    {/* value */}
+                    <p className="text-xxs font-montserrat font-bold text-white">{car.serviceAdvisor}</p>
+                </div>
+                
+                {/* ro-prw */}
+                <div className="flex items-center gap-8">
+                    {/* label */}
+                    <h3 className="text-xxs font-montserrat font-bold text-white w-22">R.O-P.R.W</h3>
+                    <hr className="w-2 h-0.5 border-white"/>
+                    {/* value */}
+                    <p className="text-xxs font-montserrat font-bold text-white">{car.RO_PRW}</p>
+                </div>
+            </div>
+
+            <div className="flex justify-center items-center mt-4 w-60 h-6 rounded-45 bg-customRed border border-solid border-white">
+                <p className="font-montserrat font-semibold text-white text-xxs">{car.work}</p>
+            </div>
+        </div>
+
+        )
+
     )
 }
 
