e3504ce6cbc1926335bacf234a943fdc7dd99724
Himanshu Rawat <himanshurawat90k@gmail.com>
2024-11-22 19:29:54 +0530

    add search button function

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,11 +1,12 @@
 import { useState, useRef, useEffect } from "react"
-import { useSearchParams, useLocation, Link } from "react-router-dom"
+import { useSearchParams, useLocation, Link, useNavigate } from "react-router-dom"
 import axios from "axios"
 import { Car } from "../context/CarContext"
 
 const SearchBar: React.FC = () => {
 
     const location = useLocation()
+    const navigate = useNavigate()
 
     //add search query to the link
     const [searchParams, setSearchParams] = useSearchParams()
@@ -117,42 +118,46 @@ const SearchBar: React.FC = () => {
                     </div>
 
                     {/* Search lens */}
-                    <svg 
-                        xmlns="http://www.w3.org/2000/svg" 
-                        width="34" 
-                        height="34" 
-                        viewBox="0 0 34 34" 
-                        fill="none"
+                    <button
+                        onClick={(e: React.MouseEvent<HTMLButtonElement>) => searchResults[0]? navigate(`/single-car-details/${searchResults[0]._id}`) : e.preventDefault()}
                     >
-                        <rect 
+                        <svg 
+                            xmlns="http://www.w3.org/2000/svg" 
                             width="34" 
-                            height="34"    
-                            fill="white"
-                        />
-                        <path 
-                            d="
-                                M29.75 29.75
-                                L23.5974 23.5974
-                                M23.5974 23.5974
-                                C24.6498 22.545 25.4847 21.2956 26.0542 19.9205
-                                C26.6238 18.5455 26.917 17.0717 26.917 15.5833
-                                C26.917 14.095 26.6238 12.6212 26.0542 11.2461
-                                C25.4847 9.87109 24.6498 8.62168 23.5974 7.56925
-                                C22.545 6.51683 21.2956 5.682 19.9205 5.11243
-                                C18.5455 4.54286 17.0717 4.24971 15.5833 4.24971
-                                C14.095 4.24971 12.6212 4.54286 11.2461 5.11243
-                                C9.87109 5.682 8.62168 6.51683 7.56925 7.5692
-                                C5.44378 9.69472 4.24971 12.5775 4.24971 15.5833
-                                C4.24971 18.5892 5.44378 21.472 7.56925 23.5974
-                                C9.69472 25.7229 12.5775 26.917 15.5833 26.917
-                                C18.5892 26.917 21.472 25.7229 23.5974 23.5974Z
-                            " 
-                            stroke="#182C83" 
-                            strokeWidth="2.5" 
-                            strokeLinecap="round" 
-                            strokeLinejoin="round"
-                        />
-                    </svg>
+                            height="34" 
+                            viewBox="0 0 34 34" 
+                            fill="none"
+                        >
+                            <rect 
+                                width="34" 
+                                height="34"    
+                                fill="white"
+                            />
+                            <path 
+                                d="
+                                    M29.75 29.75
+                                    L23.5974 23.5974
+                                    M23.5974 23.5974
+                                    C24.6498 22.545 25.4847 21.2956 26.0542 19.9205
+                                    C26.6238 18.5455 26.917 17.0717 26.917 15.5833
+                                    C26.917 14.095 26.6238 12.6212 26.0542 11.2461
+                                    C25.4847 9.87109 24.6498 8.62168 23.5974 7.56925
+                                    C22.545 6.51683 21.2956 5.682 19.9205 5.11243
+                                    C18.5455 4.54286 17.0717 4.24971 15.5833 4.24971
+                                    C14.095 4.24971 12.6212 4.54286 11.2461 5.11243
+                                    C9.87109 5.682 8.62168 6.51683 7.56925 7.5692
+                                    C5.44378 9.69472 4.24971 12.5775 4.24971 15.5833
+                                    C4.24971 18.5892 5.44378 21.472 7.56925 23.5974
+                                    C9.69472 25.7229 12.5775 26.917 15.5833 26.917
+                                    C18.5892 26.917 21.472 25.7229 23.5974 23.5974Z
+                                " 
+                                stroke="#182C83" 
+                                strokeWidth="2.5" 
+                                strokeLinecap="round" 
+                                strokeLinejoin="round"
+                            />
+                        </svg>
+                    </button>
                 </form>
             </div>
 
@@ -162,9 +167,9 @@ const SearchBar: React.FC = () => {
                 <div className="py-2 h-full px-8 flex flex-col gap-y-1 overflow-y-scroll snap-y snap-mandatory snap-always customQueryScrollbar">
                     {Array.from(searchResults, (value, index) => {
                         return (
-                            <Link to={`/single-car-details/${value._id}`} key={index} className="flex w-full justify-between snap-center items-center">
-                                <p className="font-inter text-white font-medium text-xs">{index + 1}</p>
-                                <p className="font-inter text-white font-medium text-xxs">{value.carNo}</p>
+                            <Link to={`/single-car-details/${value._id}`} key={index} className="flex w-full text-white hover:text-customRed justify-between snap-center items-center">
+                                <p className="font-inter font-medium text-xs">{index + 1}</p>
+                                <p className="font-inter font-medium text-xxs">{value.carNo}</p>
                             </Link>
                         )
                     })}
